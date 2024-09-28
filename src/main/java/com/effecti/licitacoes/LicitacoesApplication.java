818f8ab13a6c51944c3c7c43ee1e986602fbbb50
unknown <geovanni-dsc@hotmail.com>
2024-09-28 13:42:30 -0300

    Criação do Projeto e Conexão com BD SQLite

---
+++ src/main/java/com/effecti/licitacoes/LicitacoesApplication.java
@@ -0,0 +1,13 @@
+package com.effecti.licitacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicitacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicitacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/effecti/licitacoes/LicitacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.effecti.licitacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicitacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
