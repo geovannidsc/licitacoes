1d023e671bbf2d8240148f1397aeab651f212aea
unknown <geovanni-dsc@hotmail.com>
2024-10-03 02:08:17 -0300

    Feat: Configurando CORS para Vercel

--- src/main/java/com/effecti/licitacoes/config/WebConfig.java
+++ src/main/java/com/effecti/licitacoes/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:8081")
+                .allowedOrigins("http://localhost:8081","http://licitacoes-frontend.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
